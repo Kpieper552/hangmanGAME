fe4013c84f311cd4f602af65bf40e571aaab5bd1
Kpieper552 <k.pieper@novi-education.nl>
2021-02-03 13:22:42 +0100

    karinahangmangame

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
-
+// uitzoeken game laten werken isgamewon is gamelost en count! run node voor error readline syn
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
-
+  const wordSoFar = displayWordSoFar(word, guesses);
+  console.log(wordSoFar)
   const letter = question("Raad een letter: ");
-
+// require? question
   // voeg de geraden letter toe aan de array met guesses
   guesses.push(letter);
 
